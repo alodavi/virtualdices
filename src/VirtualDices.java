7901104913e27c1fdfa5d64cf9fb66313c607f75
Aloisia Davi <aloisia.davi@gmail.com>
2016-09-10 11:49:40 +0200

    first commit

--- src/VirtualDices.java
+++ src/VirtualDices.java
@@ -7,52 +7,68 @@ public class VirtualDices {
 		//let the user use the keyboard
 		Scanner scan = new Scanner(System.in);
 
-		//initialize success and reroll variables
-		int success = 0;
+
 		int reRoll = 0;
+		int totSuccess = 0;
 		
 		String confirm = "y";
 		
-		while (confirm == "y"){
-			
-			//number of dices to be rolled
-			int dicesNum;
-			if (reRoll != 0){
+		try{	
+			while (confirm.equalsIgnoreCase("y")){
 				
-				System.out.println("You roll "+reRoll+" dices.");
-				dicesNum = reRoll;
-			} else {
-				System.out.println("How many dices do you want to roll? ");
-				dicesNum = scan.nextInt();
-			}
-
-			
-			for(int x=0; x<dicesNum; x++){
+				//initialize success and reroll variables
+				int success = 0;
 				
 				
-				int rolledDice = (int)(Math.random()*10 + 1);
-				if (rolledDice >= 8){
-					success++;
-					if (rolledDice==10){
-						reRoll++;
-					}
+				//number of dices to be rolled
+				int dicesNum;
+				
+				
+				if (reRoll != 0){
+					
+					System.out.println("You roll "+reRoll+" dices.");
+					dicesNum = reRoll;
+					reRoll = 0;
+				} else {
+					System.out.println("How many dices do you want to roll? ");
+					dicesNum = scan.nextInt();	
 				}
-				System.out.println(rolledDice);
 				
+				for(int x=0; x<dicesNum; x++){
+					
+	
+					
+					int rolledDice = (int)(Math.random()*10 + 1);
+					if (rolledDice >= 8){
+						success++;
+						//totSuccess += success;
+						if (rolledDice==10){
+							reRoll++;
+						}
+					}
+					System.out.println("You rolled: " + rolledDice);
+					
+				}
+				System.out.println(success + " successes.");
+				totSuccess += success;
+				System.out.println(totSuccess + " in total.");
+				if (reRoll != 0){
+					System.out.println("Reroll "+reRoll +".");
+					System.out.println("Do you continue (y/n)? ");
+					confirm = scan.next();
+					//dicesNum = reRoll;
+					//reRoll = 0;
+				} else {
+					System.out.println("Play again (y/n)? ");
+					confirm = scan.next();
+					totSuccess = 0;
+					
+				}
+	
 			}
-			System.out.println(success + " successes.");
-			if (reRoll != 0){
-				System.out.println("Reroll "+reRoll +".");
-				System.out.println("Do you continue (y/n)? ");
-				confirm = scan.next();
-				dicesNum = reRoll;
-			} else {
-				System.out.println("Play again (y/n)? ");
-				confirm = scan.next();
-				
-			}
+		} catch(Exception e){
+			System.out.println("Invalid input. Please restart the program.");
 		}
-		
 	}
 
 }
